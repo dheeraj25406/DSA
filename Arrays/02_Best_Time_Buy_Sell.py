18951d3fc3e96f77803cde2b01ccc6c8cda93646
Alamuri Dheeraj <wownice254@gmail.com>
2025-07-13 17:20:44 +0530

    Update 02_Best_Time_Buy_Sell.py

--- Arrays/02_Best_Time_Buy_Sell.py
+++ Arrays/02_Best_Time_Buy_Sell.py
@@ -1,1 +1,27 @@
+# Problem: Best Time to Buy and Sell Stock
+# LeetCode: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+# Approach: Track min-so-far and max profit
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int k=0;
+        int min=prices[0];
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]<min){
+                min=prices[i];
+            }
+            else{
+                int temk=prices[i]-min;
+                if(temk>k) k=temk;             
+            }
+        }
+        return k;
+        
+    }
+};
