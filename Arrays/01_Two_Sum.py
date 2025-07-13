b5a90279a17d491ef51e28f6ae5513f100872df2
Alamuri Dheeraj <wownice254@gmail.com>
2025-07-13 17:18:48 +0530

    Update 01_Two_Sum.py

--- Arrays/01_Two_Sum.py
+++ Arrays/01_Two_Sum.py
@@ -1,1 +1,23 @@
+#Problem : Two Sum
+#Leetcode : https://leetcode.com/problems/two-sum/
+#Approach : Brute Force, Optimized solution will be added later
+#Time Complexity : O(n^2)
+#Space Complexity : O(1)
 
+
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        for (int i=0;i<n-1;i++) {
+            for (int j=i+1;j<n;j++) {
+                if (nums[i]+nums[j]==target) {
+                    return {i,j};
+                }
+            }
+        }
+        return {};
+    }
+};
