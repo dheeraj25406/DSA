49b0b379135483a6a3a99695f2738e37962ba0d2
Alamuri Dheeraj <wownice254@gmail.com>
2025-07-20 00:01:03 +0530

    Update 13_Reverse_Linked_List.py

--- LinkedList/13_Reverse_Linked_List.py
+++ LinkedList/13_Reverse_Linked_List.py
@@ -1,1 +1,32 @@
+# LeetCode: https://leetcode.com/problems/reverse-linked-list/
+# TC: O(n) – one pass through the list
+# SC: O(1) – reversing in-place
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* revers(ListNode* h){
+        if(h==NULL || h->next==NULL){
+            return h;
+        }
+        ListNode* ch=revers(h->next);
+        h->next->next=h;
+        h->next=NULL;
+
+        return ch;
+    }
+    ListNode* reverseList(ListNode* head) {
+
+        return revers(head);
+        
+    }
+};
