54040bcdf1b4fe389f8f5480761ff547e323bb58
Alamuri Dheeraj <wownice254@gmail.com>
2025-07-23 23:49:26 +0530

    Create 02_Merge_Sorted_Array.cpp

---
+++ Sorting_And_Strings/02_Merge_Sorted_Array.cpp
@@ -0,0 +1,34 @@
+/* Link: Leetcode 88
+Time Complexity: O(m + n)
+Space Complexity: O(1)
+Approach: Merge from end using two pointers */
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> ans;
+        int i=0,j=0;
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+                ans.push_back(nums1[i]);
+                i++;
+            }
+            else{
+                ans.push_back(nums2[j]);
+                j++;
+            }
+        }
+        while(i<m){
+            ans.push_back(nums1[i]);
+            i++;
+        }
+        while(j<n){
+            ans.push_back(nums2[j]);
+            j++;
+        }
+        for(int i=0;i<ans.size();i++){
+            nums1[i]=ans[i];
+        }
+    }
+};
+
