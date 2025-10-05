917595650c56c17dac67a4e7ad21e00ce1ef873a
Alamuri Dheeraj <wownice254@gmail.com>
2025-10-05 10:53:22 +0530

    Create 04_Preorder_Reversal.cpp

---
+++ Binary_Search_Trees/04_Preorder_Reversal.cpp
@@ -0,0 +1,27 @@
+#Approach : Depth First Search
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void temp(TreeNode* root, vector<int> &v){
+        if(!root)   return;
+        v.push_back(root->val);
+        temp(root->left,v);
+        temp(root->right,v);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>v;
+        temp(root,v);
+        return v;
+        
+    }
+};
