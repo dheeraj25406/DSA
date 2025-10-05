8804edea8ffd85e575ba1fec9e987c62bc909ce0
Alamuri Dheeraj <wownice254@gmail.com>
2025-10-05 11:07:50 +0530

    Create 11_ZigZag_Traversal.cpp

---
+++ Binary_Search_Trees/11_ZigZag_Traversal.cpp
@@ -0,0 +1,31 @@
+//LC 103
+//Apprach: Breadth First Search
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool cmp(TreeNode* root1,TreeNode* root2){
+        if(!root1 || !root2)  return root1==root2;
+        else if(root1 && root2){
+            if(root1->val == root2->val){
+            return cmp(root1->left,root2->right) && cmp(root1->right,root2->left);
+            }
+            return false;
+        }
+        return false;
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(!root)   return true;
+        return cmp(root->left,root->right);
+        
+    }
+};
