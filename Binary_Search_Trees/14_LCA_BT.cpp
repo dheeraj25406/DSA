78e8944f38fd346ba31903f4d955ddc2a1801fc3
Alamuri Dheeraj <wownice254@gmail.com>
2025-10-05 11:15:20 +0530

    Create 14_LCA_BT.cpp

---
+++ Binary_Search_Trees/14_LCA_BT.cpp
@@ -0,0 +1,25 @@
+//LC 236
+//Approach : DFS
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root)   return NULL;
+        if(root==p || root==q)  return root;
+        TreeNode* l=lowestCommonAncestor(root->left,p,q);
+        TreeNode* r=lowestCommonAncestor(root->right,p,q);
+        if(r && l)  return root;
+        else if(!r && l)    return l;
+        else if(r && !l)    return r;
+        else    return NULL;
+    }
+};
