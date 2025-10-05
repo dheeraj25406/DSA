3208ae091e51b48082e78211d57872198729131d
Alamuri Dheeraj <wownice254@gmail.com>
2025-10-05 11:17:15 +0530

    Create 16_BT_Inorder.cpp

---
+++ Binary_Search_Trees/16_BT_Inorder.cpp
@@ -0,0 +1,40 @@
+//LC 94
+// Approach : DFS
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> v;
+        TreeNode* curr=root;
+        while(curr){
+            if(!curr->left){
+                v.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                TreeNode* prev=curr->left;
+                while(prev->right && prev->right!=curr) prev=prev->right;
+                if(!prev->right){
+                    prev->right=curr;
+                    curr=curr->left;
+                }
+                else{
+                    prev->right=nullptr;
+                    v.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+        }
+        return v;
+    }
+};
