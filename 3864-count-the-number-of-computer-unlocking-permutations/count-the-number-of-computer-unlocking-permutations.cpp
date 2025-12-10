76cb6b80e84fc1456356dffd6e776c10b1decddc
Dheeraj Alamuri <wownice254@gmail.com>
2025-12-10 12:50:44 +0530

    Time: 0 ms (100.00%) | Memory: 92.8 MB (47.00%) - LeetSync

--- 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-    const int mod=1e9+7;
-    long long fact(long long n){
-        if(n==0)    return 1LL;
-        else    return n*(fact(n-1)%mod)%mod;
-        ;
-    }
-    int countPermutations(vector<int>& c) {
-        unordered_map<long long,int> m;
-        for(int i=0;i<c.size();i++) m[c[i]]++;
-        long long a=c[0];
-        long long ans=0;
-        for(int i=1;i<c.size();i++) if(c[i]<=a) return 0;
-        ans=fact(c.size()-1);
+    int countPermutations(vector<int>& a) {
+        int n=a.size(),mod=1e9+7;
+        long long ans=1;
+        for(int i=1;i<n;i++){
+            if(a[i]<=a[0])return 0;
+            ans=ans*i%mod;
+        }
         return ans;
     }
 };
