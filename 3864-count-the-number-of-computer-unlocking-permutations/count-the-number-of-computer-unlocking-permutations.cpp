777c02169376e0cba9509ed8023ad1fe69108bb5
Dheeraj Alamuri <wownice254@gmail.com>
2025-12-10 12:48:18 +0530

    Time: 85 ms (5.00%) | Memory: 126.8 MB (5.00%) - LeetSync

---
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    const int mod=1e9+7;
+    long long fact(long long n){
+        if(n==0)    return 1LL;
+        else    return n*(fact(n-1)%mod)%mod;;
+    }
+    int countPermutations(vector<int>& c) {
+        unordered_map<long long,int> m;
+        for(int i=0;i<c.size();i++) m[c[i]]++;
+        long long a=c[0];
+        long long ans=0;
+        for(int i=1;i<c.size();i++) if(c[i]<=a) return 0;
+        ans=fact(c.size()-1);
+        return ans;
+    }
+};
