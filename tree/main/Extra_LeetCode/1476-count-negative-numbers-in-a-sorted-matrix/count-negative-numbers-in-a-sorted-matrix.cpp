54389d49f4861dd118b41885490dc4396891fb0e
Dheeraj Alamuri <wownice254@gmail.com>
2025-12-28 09:06:57 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (67.95%) - LeetSync

---
+++ tree/main/Extra_LeetCode/1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& g) {
+        int ans=0;
+        for(int i=0;i<g.size();i++){
+            for(int j=0;j<g[0].size();j++){
+                if(g[i][j]<0)  ans++;
+            }
+        }
+        return ans;
+    }
+};
