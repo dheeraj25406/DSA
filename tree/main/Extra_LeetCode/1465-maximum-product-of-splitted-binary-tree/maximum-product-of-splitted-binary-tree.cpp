3d0696249529668a6baa6b3831a98507ceac2491
Dheeraj Alamuri <wownice254@gmail.com>
2026-01-07 19:51:02 +0530

    Time: 0 ms (100.00%) | Memory: 78.1 MB (73.71%) - LeetSync

--- tree/main/Extra_LeetCode/1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
+++ tree/main/Extra_LeetCode/1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
@@ -10,53 +10,21 @@
  * };
  */
 class Solution {
-    unordered_map<TreeNode*, long long> mp;
-    long long totalSum = 0;
-    long long ans = 0;
-    long long MOD = 1e9 + 7;
 public:
-
-    int findSum(TreeNode* root){
-        if(root == NULL){
-            return 0;
-        }
-        
-        totalSum += root->val;
-        
-        long long val = root->val + findSum(root->left) + findSum(root->right);
-        
-        return mp[root] = val;
+    long long sm=0,ans=0;
+    long long som(TreeNode* root){
+        if(!root)   return 0;
+        return root->val+som(root->left)+som(root->right);
     }
-    
-    void solve(TreeNode* root){
-        if(root == NULL){
-            return;
-        }
-        
-        if(root->left){
-            long long s1 = totalSum - mp[root->left];
-            long long s2 = mp[root->left];
-            
-            ans = max(ans, s1*s2);
-            
-            solve(root->left);
-        }
-        
-        if(root->right){
-            long long s1 = totalSum - mp[root->right];
-            long long s2 = mp[root->right];
-            
-            ans = max(ans, s1*s2);
-            
-            solve(root->right);
-        }
+    int solve(TreeNode* root){
+        if(!root)   return 0;
+        long long tmp=root->val+solve(root->left)+solve(root->right);
+        ans=max(ans,tmp*(sm-tmp));
+        return tmp;
     }
-
     int maxProduct(TreeNode* root) {
-        findSum(root);
-        
+        sm=som(root);
         solve(root);
-        
-        return ans % MOD;
+        return ans%1000000007;
     }
 };
