3bf39f100a333c3cf0b65d7b1ae7f4200386dcb9
Dheeraj Alamuri <wownice254@gmail.com>
2026-03-09 21:28:34 +0530

    Time: 110 ms (81.30%) | Memory: 51.2 MB (34.15%) - LeetSync

---
+++ tree/main/Extra_LeetCode/3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numberOfStableArrays(int zero,int one,int limit){
+        int mod=1e9+7;
+        vector<vector<vector<long long>>> dp(zero+1,vector<vector<long long>>(one+1,vector<long long>(2)));
+        for(int i=1;i<=min(zero,limit);i++)dp[i][0][0]=1;
+        for(int j=1;j<=min(one,limit);j++)dp[0][j][1]=1;
+        for(int i=1;i<=zero;i++){
+            for(int j=1;j<=one;j++){
+                long long x=(i-limit-1>=0?dp[i-limit-1][j][1]:0);
+                long long y=(j-limit-1>=0?dp[i][j-limit-1][0]:0);
+                dp[i][j][0]=(dp[i-1][j][0]+dp[i-1][j][1]-x)%mod;
+                dp[i][j][1]=(dp[i][j-1][0]+dp[i][j-1][1]-y)%mod;
+                if(dp[i][j][0]<0)dp[i][j][0]+=mod;
+                if(dp[i][j][1]<0)dp[i][j][1]+=mod;
+            }
+        }
+        return (dp[zero][one][0]+dp[zero][one][1])%mod;
+    }
+};
