b323f577ff541203164465b6995091775d92b295
Dheeraj Alamuri <wownice254@gmail.com>
2026-04-07 22:07:38 +0530

    feat(codeforces): solve Right Maximum [C++17 (GCC 7-32)]

--- CodeForces/Medium/Right-Maximum.cpp
+++ CodeForces/Medium/Right-Maximum.cpp
@@ -8,12 +8,12 @@
 - Tags: N/A
 - Runtime: 78 ms
 - Memory: 100 KB
-- Solved At: 2026-04-07T15:34:28.615Z
+- Solved At: 2026-04-07T16:37:36.989Z
 
 ## Notes
 No notes added.
 
 ## Solution
 ```
-// No code captured. Use manual push after opening submission.
+#include <bits/stdc++.h>using namespace std; int main(){    ios::sync_with_stdio(false);    cin.tie(NULL);    int t;    cin>>t;    while(t--){        int n;        cin>>n;        vector<int>a(n+1),last(n+1),jump(n+1);        int mx=0;        for(int i=1;i<=n;i++){            cin>>a[i];            last[a[i]]=i;            mx=max(mx,a[i]);            jump[i]=last[mx];        }        int len=n,ans=0;        while(len>0){            int k=jump[len];            len=k-1;            ans++;        }        cout<<ans<<"\n";    }}
 ```
