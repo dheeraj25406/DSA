4d512514d52e93a17d6e62ed4a18e388077039f2
Dheeraj Alamuri <wownice254@gmail.com>
2026-04-07 22:17:11 +0530

    feat(codeforces): solve Right Maximum [C++17 (GCC 7-32)]

--- CodeForces/Medium/Right-Maximum.cpp
+++ CodeForces/Medium/Right-Maximum.cpp
@@ -8,12 +8,39 @@
 - Tags: N/A
 - Runtime: 78 ms
 - Memory: 100 KB
-- Solved At: 2026-04-07T16:45:04.642Z
+- Solved At: 2026-04-07T16:47:10.960Z
 
 ## Notes
 No notes added.
 
 ## Solution
 ```
-#include <bits/stdc++.h>using namespace std; int main(){    ios::sync_with_stdio(false);    cin.tie(NULL);    int t;    cin>>t;    while(t--){        int n;        cin>>n;        vector<int>a(n+1),last(n+1),jump(n+1);        int mx=0;        for(int i=1;i<=n;i++){            cin>>a[i];            last[a[i]]=i;            mx=max(mx,a[i]);            jump[i]=last[mx];        }        int len=n,ans=0;        while(len>0){            int k=jump[len];            len=k-1;            ans++;        }        cout<<ans<<"\n";    }}
+#include <bits/stdc++.h>using namespace std;
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  cin>>t;
+  while(t--) {
+    int n;
+    cin>>n;
+    vector<int>a(n+1),last(n+1),jump(n+1);
+    int mx=0;
+    for(int i=1;
+    i<=n;
+    i++) {
+      cin>>a[i];
+      last[a[i]]=i;
+      mx=max(mx,a[i]);
+      jump[i]=last[mx];
+    }
+    int len=n,ans=0;
+    while(len>0) {
+      int k=jump[len];
+      len=k-1;
+      ans++;
+    }
+    cout<<ans<<"\n";
+  }
+}
 ```
