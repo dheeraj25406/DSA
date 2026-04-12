ff8f1ca1f619d6840435cd4c6a3963fac94f1f91
Dheeraj Alamuri <wownice254@gmail.com>
2026-04-12 16:38:23 +0530

    feat(codeforces): solve Personal submissions [C++]

--- CodeForces/Easy/Personal-submissions.cpp
+++ CodeForces/Easy/Personal-submissions.cpp
@@ -7,63 +7,51 @@
 - Status: Accepted
 - Runtime: 78 ms
 - Memory: 100 KB
-- Solved At: 2026-04-12T11:02:27.095Z
+- Solved At: 2026-04-12T11:08:22.695Z
 
 ## Code
 ```cpp
-#include <algorithm>
 #include <bits/stdc++.h>
 using namespace std;
- #define int long long#define vi vector<ll>#define vvi vector<vi>#define pll pair<ll, ll>#define vp vector<pll>#define fastio() ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-cout.tie(NULL)#define M 1000000007// #define M 998244353#define INF 1e18#define endl "\n"#define pb push_back#define ppb pop_back#define mp make_pair#define ff first#define ss second#define fr(i,s,e) for(ll i = s;
- i < e;
- i++)#define forrr(i,s,e) for(ll i = s;
- i >= e;
- i--)#define sz(x) ((int)(x).size())#define all(x) (x).begin(), (x).end()#define yn(f) f?cout<<"YES\n":cout<<"NO\n"#define syn(f) f?cout<<"Yes\n":cout<<"No\n" typedef long long ll;
-typedef unsigned long long ull;
-typedef long double lld;
-#ifdef ONLINE_JUDGE#define debug(x) #else#define debug(x) cerr << #x <<" ";
- _print(x);
- cerr << endl;
-#endif   signed main() {
-    fastio();
-    // factCompute(1e6);
-    // sieve(1e7);
-    ll t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        string s;
-        cin>>s;
-        vector<int>z,o;
-        for(int i=0;
-i<n;
-i++){
-            if(s[i]=='0')   z.push_back(i+1);
-            else o.push_back(i+1);
-        
+ class Solution{
+public:    long long lcm2(long long a,long long b){
+        return a/__gcd(a,b)*b;
+    
 }
-         if(o.size()%2==0){
-            cout<<o.size()<<"\n";
-            for(int x:o)    cout<<x<<" ";
-            cout<<"\n";
-        
-}
-        else if(z.size()%2==1){
-            cout<<z.size()<<"\n";
-            for(int x:z)    cout<<x<<" ";
-            cout<<"\n";
-        
-}
-        else{
-            cout<<-1<<"\n";
+        void solve(){
+        int t;
+cin>>t;
+        while(t--){
+            long long a,b,c,m;
+cin>>a>>b>>c>>m;
+            long long A=m/a,B=m/b,C=m/c;
+            long long ab=lcm2(a,b);
+            long long ac=lcm2(a,c);
+            long long bc=lcm2(b,c);
+            long long abc=lcm2(ab,c);
+            long long AB=m/ab,AC=m/ac,BC=m/bc,ABC=m/abc;
+            long long onlyA=A-AB-AC+ABC;
+            long long onlyB=B-AB-BC+ABC;
+            long long onlyC=C-AC-BC+ABC;
+            long long AB2=AB-ABC;
+            long long AC2=AC-ABC;
+            long long BC2=BC-ABC;
+            long long ansA=onlyA*6+AB2*3+AC2*3+ABC*2;
+            long long ansB=onlyB*6+AB2*3+BC2*3+ABC*2;
+            long long ansC=onlyC*6+AC2*3+BC2*3+ABC*2;
+                    cout<<ansA<<" "<<ansB<<" "<<ansC<<"\n";
         
 }
     
 }
-    return 0;
+
+}
+;
+ int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+     Solution s;
+    s.solve();
 
 }
 ```
