8a2c3c8de4fae3ae841bf9e634ad37b393ba3f8f
Dheeraj Alamuri <wownice254@gmail.com>
2026-04-12 11:05:34 +0530

    feat(unknown): solve Unknown Problem [C++]

--- Other/Unknown/Unknown-Problem.cpp
+++ Other/Unknown/Unknown-Problem.cpp
@@ -1,13 +1,13 @@
 # Unknown Problem
 
 - Platform: Unknown
-- URL: https://leetcode.com/problems/container-with-most-water/submissions/1975095904/
+- URL: https://www.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1
 - Difficulty: Unknown
 - Language: C++
 - Status: Accepted
 - Runtime: N/A
 - Memory: N/A
-- Solved At: 2026-04-12T05:35:18.684Z
+- Solved At: 2026-04-12T05:35:33.435Z
 
 ## Code
 ```cpp
