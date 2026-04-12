a90d32df2cece03d050aec4b322318bec2d7b153
Dheeraj Alamuri <wownice254@gmail.com>
2026-04-12 19:45:18 +0530

    feat(leetcode): solve Special Binary String [C++]

--- LeetCode/Hard/Special-Binary-String.cpp
+++ LeetCode/Hard/Special-Binary-String.cpp
@@ -1,31 +1,33 @@
 # Special Binary String
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/special-binary-string/submissions/1976165694/
+- URL: https://leetcode.com/problems/special-binary-string/submissions/1976461874/
 - Difficulty: Hard
 - Language: C++
 - Status: Accepted
-- Runtime: 0 ms
-- Memory: 9.6 MB
-- Solved At: 2026-04-12T13:42:38.238Z
+- Runtime: 2
+ms
+- Memory: 9.40
+MB
+- Solved At: 2026-04-12T14:15:17.150Z
 
 ## Code
 ```cpp
-1class Solution {
-2public:
-3    string makeLargestSpecial(string s) {
-4        if (s == "") return s;
-5        vector<string> ans;
-6        int cnt = 0;
-7        for (int i = 0, j = 0; i < s.size(); ++i) {
-8            cnt += s[i] == '1' ? 1 : -1;
-9            if (cnt == 0) {
-10                ans.push_back("1" + makeLargestSpecial(s.substr(j + 1, i - j - 1)) + "0");
-11                j = i + 1;
-12            }
-13        }
-14        sort(ans.begin(), ans.end(), greater<string>{});
-15        return accumulate(ans.begin(), ans.end(), ""s);
-16    }
-17};
+class Solution {
+public:
+    string makeLargestSpecial(string s) {
+        if (s == "") return s;
+        vector<string> ans;
+        int cnt = 0;
+        for (int i = 0, j = 0; i < s.size(); ++i) {
+            cnt += s[i] == '1' ? 1 : -1;
+            if (cnt == 0) {
+                ans.push_back("1" + makeLargestSpecial(s.substr(j + 1, i - j - 
+1)) + "0");
+                j = i + 1;
+            }
+        }
+        sort(ans.begin(), ans.end(), greater<string>{});
+        return accumulate(ans.begin(), ans.end(), ""s);
+    }
 ```
