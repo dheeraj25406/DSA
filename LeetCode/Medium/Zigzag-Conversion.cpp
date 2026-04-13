47027981e4e9c1e253a80993ec933bf7a98977f8
Dheeraj Alamuri <wownice254@gmail.com>
2026-04-14 02:39:11 +0530

    feat(leetcode): solve Zigzag Conversion [C++]

--- LeetCode/Medium/Zigzag-Conversion.cpp
+++ LeetCode/Medium/Zigzag-Conversion.cpp
@@ -1,20 +1,19 @@
 # Zigzag Conversion
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/zigzag-conversion/submissions/1977724922/
+- URL: https://leetcode.com/problems/zigzag-conversion/submissions/1977731723/
 - Difficulty: Medium
 - Language: C++
 - Status: Accepted
-- Runtime: 0 ms
-- Memory: 10.79
+- Runtime: 3
+ms
+- Memory: 10.87
 MB
-- Solved At: 2026-04-13T20:53:20.111Z
+- Solved At: 2026-04-13T21:09:07.154Z
 
 ## Code
 ```cpp
-string result = "";
-        int n = s.length();
-        int cycle = 2 * (numRows - 1);
+int cycle = 2 * (numRows - 1);
         for (int row = 0; row < numRows; row++) {
             for (int j = row; j < n; j += cycle) {
                 result += s[j];
