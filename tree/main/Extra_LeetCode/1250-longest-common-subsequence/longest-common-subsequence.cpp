342c928e86c855e623527e072989e2e2cb45c02a
Dheeraj Alamuri <wownice254@gmail.com>
2026-05-03 12:46:55 +0530

    Time: 43 ms (21.66%) | Memory: 27.7 MB (13.34%) - LeetSync

---
+++ tree/main/Extra_LeetCode/1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> t;
+    int solve(string &s1,string &s2, int n,int m){
+        if(n==0 || m==0)    return 0;
+        if(t[n][m]!=-1)  return t[n][m];
+        if(s1[n-1]==s2[m-1])    return t[n][m]=solve(s1,s2,n-1,m-1)+1;
+        else    return t[n][m]=max(solve(s1,s2,n-1,m),solve(s1,s2,n,m-1));
+    }
+
+    int longestCommonSubsequence(string s1, string s2) {
+        int n=s1.length(),m=s2.length();
+        t.assign(n+1,vector<int>(m+1,-1));
+        int ans=solve(s1,s2,n,m);
+        return ans;
+    }
+};
