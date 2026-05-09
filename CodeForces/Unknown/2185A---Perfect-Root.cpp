5026d708e207c1ae343462bfa1665246580fcdd5
Dheeraj Alamuri <wownice254@gmail.com>
2026-05-09 18:57:27 +0530

    feat(codeforces): solve 2185A - Perfect Root [C++]

---
+++ CodeForces/Unknown/2185A---Perfect-Root.cpp
@@ -0,0 +1,69 @@
+# 2185A - Perfect Root
+
+- Platform: CodeForces
+- URL: https://codeforces.com/problemset/problem/2185/A
+- Difficulty: Unknown
+- Language: C++
+- Status: Accepted
+- Runtime: 31 ms
+- Memory: 0 KB
+- Solved At: 2026-05-09T13:27:27.145Z
+
+## Code
+```cpp
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<ll>
+#define vvi vector<vi>
+#define pll pair<ll, ll>
+#define vp vector<pll>
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define M 1000000007
+// #define M 998244353
+#define INF 1e18
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define fr(i,s,e) for(ll i = s; i < e; i++)
+#define forrr(i,s,e) for(ll i = s; i >= e; i--)
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define yn(f) f?cout<<"YES\n":cout<<"NO\n"
+#define syn(f) f?cout<<"Yes\n":cout<<"No\n"
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+#ifdef ONLINE_JUDGE
+#define debug(x) 
+#else
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#endif
+
+
+
+signed main() {
+    fastio();
+    // factCompute(1e6);
+    // sieve(1e7);
+    ll t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        fr(i,1,n+1){
+            cout<<i;
+            if(i<n) cout<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+
+}
+```
