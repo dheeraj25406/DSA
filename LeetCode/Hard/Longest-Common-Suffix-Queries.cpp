5370dff1d5f7f8f9efd79d1279dcd904ece1107f
Dheeraj Alamuri <wownice254@gmail.com>
2026-05-29 13:54:47 +0530

    feat(leetcode): solve Longest Common Suffix Queries [C++]

---
+++ LeetCode/Hard/Longest-Common-Suffix-Queries.cpp
@@ -0,0 +1,32 @@
+# Longest Common Suffix Queries
+
+- Platform: LeetCode
+- URL: https://leetcode.com/problems/longest-common-suffix-queries/submissions/2016233047/?envType=daily-question&envId=2026-05-29
+- Difficulty: Hard
+- Language: C++
+- Status: Accepted
+- Runtime: 0 ms
+- Memory: 358.94
+MB
+- Solved At: 2026-05-29T08:24:46.544Z
+
+## Code
+```cpp
+reverse(it.begin(), it.end());
+        }
+        for (auto& it : wq) {
+            k++;
+            }
+                minlen = it.size();
+            if (it.size() < minlen) {
+                minind = k;
+        for (auto& it : wc) {
+            reverse(it.begin(), it.end());
+        int minind = -1;
+        int k = 0;
+class Solution {
+public:
+    vector<int> stringIndices(vector<string>& wc, vector<string>& wq) {
+        int minlen = INT_MAX;
+        }
+```
